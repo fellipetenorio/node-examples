c54f23a65dd4edef2ccf06c20d5d3a08d72e3c7b
FTF <>
2018-11-12 00:23:09 -0300

    integrate with bcryptjs

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,14 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
+var p = '123';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+});
 
-var token = jwt.sign(data, '123abc')
-console.log(token);
+// var hashedPassword = '$2a$10$qGuoj/8PB2dEEtbHRkMtseElfYgAmoFsMzmYDU3hEJjCj4ZaNqjWC';
+// bcrypt.compare(password, hashedPassword, (err, res) => {
+//     console.log(res);
+// })
 
-var decoded = jwt.verify(token+'s', '123abc')
-    .then(decoded => console.log(decoded))
-    .catch(err => console.log('laksdjfasdlkfjadslkfj', err));
-//console.log(decoded);
+// bcrypt.compare(p, hashedPassword, (err, res) => {
+//     console.log(res);
+// })
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc')
+// console.log(token);
+
+// var decoded = jwt.verify(token+'s', '123abc')
+//     .then(decoded => console.log(decoded))
+//     .catch(err => console.log('laksdjfasdlkfjadslkfj', err));
+// //console.log(decoded);
