c1fead0b6d9ad44c7b1499a5a93f00bd65b08996
Fellipe Tenorio Ferrer <fellipe.tenorio@gmail.com>
2018-11-13 15:15:39 -0200

    initial socket files

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,16 +1,23 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '..', 'public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
+const publicPath = path.join(__dirname, '..', 'public');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.send('hi');
-// })
+io.on('connection', socket => {
+    console.log('new user connected');
+    socket.on('disconnect', () => {
+        console.log('client disconnect');
+    });
+});
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
-})
+});
